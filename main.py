ff119682a10ca9077517b23a08cfde5864ad9fd0
Antoine Thomas <84747640+antothom@users.noreply.github.com>
2024-10-13 17:33:35 +0100

    Removed if-statement

--- main.py
+++ main.py
@@ -26,57 +26,56 @@ def main():
     airtable_manager = AirtableManager()
 
     for i in fetched_data:
-        if i == 'Techstars Startup Digest Portugal':
-            if fetched_data[i].empty:
-                print(f"\033[93mWARNING: No new data for {i}\033[0m")
-                continue
-            prompt_filename = "config/prompts/prompt_" + i.lower().replace(" ", "_") + ".txt"
-            prompt = open(prompt_filename, "r").read()
+        if fetched_data[i].empty:
+            print(f"\033[93mWARNING: No new data for {i}\033[0m")
+            continue
+        prompt_filename = "config/prompts/prompt_" + i.lower().replace(" ", "_") + ".txt"
+        prompt = open(prompt_filename, "r").read()
 
-            news_df = pd.DataFrame(columns=['Title', 'Summary', 'Link', 'Source', 'Published'])
-            events_df = pd.DataFrame(
-                columns=['Title', 'Summary', 'Link', 'Start Date', 'End Date', 'Source', 'Published'])
-            jobs_df = pd.DataFrame(columns=['Position', 'Company', 'Location', 'Link', 'Source', 'Published'])
-            resources_df = pd.DataFrame(columns=['Title', 'Summary', 'Link', 'Source', 'Published'])
-            all_dfs = {'News': news_df, 'Events': events_df, 'Jobs': jobs_df, 'Resources': resources_df}
+        news_df = pd.DataFrame(columns=['Title', 'Summary', 'Link', 'Source', 'Published'])
+        events_df = pd.DataFrame(
+            columns=['Title', 'Summary', 'Link', 'Start Date', 'End Date', 'Source', 'Published'])
+        jobs_df = pd.DataFrame(columns=['Position', 'Company', 'Location', 'Link', 'Source', 'Published'])
+        resources_df = pd.DataFrame(columns=['Title', 'Summary', 'Link', 'Source', 'Published'])
+        all_dfs = {'News': news_df, 'Events': events_df, 'Jobs': jobs_df, 'Resources': resources_df}
 
-            for j in fetched_data[i].index:
-                extractor = DataExtractor(fetched_data[i].loc[j, 'content'], fetched_data[i].loc[j, 'published'],
-                                          fetched_data[i].loc[j, 'source'], prompt)
-                extracted = extractor.extract()
-                curr_dfs = extractor.text_to_df(extracted)
-                for k in curr_dfs:
-                    all_dfs[k] = pd.concat([all_dfs[k], curr_dfs[k]], ignore_index=True)
+        for j in fetched_data[i].index:
+            extractor = DataExtractor(fetched_data[i].loc[j, 'content'], fetched_data[i].loc[j, 'published'],
+                                      fetched_data[i].loc[j, 'source'], prompt)
+            extracted = extractor.extract()
+            curr_dfs = extractor.text_to_df(extracted)
+            for k in curr_dfs:
+                all_dfs[k] = pd.concat([all_dfs[k], curr_dfs[k]], ignore_index=True)
 
-            for i in all_dfs:
-                for j in all_dfs[i].index:
-                    if i == 'News':
-                        airtable_manager.add_news(all_dfs[i].loc[j, 'Title'],
+        for i in all_dfs:
+            for j in all_dfs[i].index:
+                if i == 'News':
+                    airtable_manager.add_news(all_dfs[i].loc[j, 'Title'],
+                                              all_dfs[i].loc[j, 'Summary'],
+                                              all_dfs[i].loc[j, 'Link'],
+                                              all_dfs[i].loc[j, 'Source'],
+                                              all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
+                elif i == 'Events':
+                    airtable_manager.add_event(all_dfs[i].loc[j, 'Title'],
+                                               all_dfs[i].loc[j, 'Summary'],
+                                               all_dfs[i].loc[j, 'Link'],
+                                               all_dfs[i].loc[j, 'Start Date'],
+                                               all_dfs[i].loc[j, 'End Date'],
+                                               all_dfs[i].loc[j, 'Source'],
+                                               all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
+                elif i == 'Jobs':
+                    airtable_manager.add_job(all_dfs[i].loc[j, 'Position'],
+                                             all_dfs[i].loc[j, 'Company'],
+                                             all_dfs[i].loc[j, 'Location'],
+                                             all_dfs[i].loc[j, 'Link'],
+                                             all_dfs[i].loc[j, 'Source'],
+                                             all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
+                elif i == 'Resources':
+                    airtable_manager.add_resource(all_dfs[i].loc[j, 'Title'],
                                                   all_dfs[i].loc[j, 'Summary'],
                                                   all_dfs[i].loc[j, 'Link'],
                                                   all_dfs[i].loc[j, 'Source'],
                                                   all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
-                    elif i == 'Events':
-                        airtable_manager.add_event(all_dfs[i].loc[j, 'Title'],
-                                                   all_dfs[i].loc[j, 'Summary'],
-                                                   all_dfs[i].loc[j, 'Link'],
-                                                   all_dfs[i].loc[j, 'Start Date'],
-                                                   all_dfs[i].loc[j, 'End Date'],
-                                                   all_dfs[i].loc[j, 'Source'],
-                                                   all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
-                    elif i == 'Jobs':
-                        airtable_manager.add_job(all_dfs[i].loc[j, 'Position'],
-                                                 all_dfs[i].loc[j, 'Company'],
-                                                 all_dfs[i].loc[j, 'Location'],
-                                                 all_dfs[i].loc[j, 'Link'],
-                                                 all_dfs[i].loc[j, 'Source'],
-                                                 all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
-                    elif i == 'Resources':
-                        airtable_manager.add_resource(all_dfs[i].loc[j, 'Title'],
-                                                      all_dfs[i].loc[j, 'Summary'],
-                                                      all_dfs[i].loc[j, 'Link'],
-                                                      all_dfs[i].loc[j, 'Source'],
-                                                      all_dfs[i].loc[j, 'Published'].strftime('%Y-%m-%d'))
 
 
 if __name__ == "__main__":
